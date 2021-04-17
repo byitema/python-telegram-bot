613ab71a42f1c634628c943116b02a8c06c5e350
Artem Bulakh <artem.bulakh1@gmail.com>
2021-04-18 01:27:49 +0300

    add choice functionality

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 import telebot
 import random
+import re
 
 
 with open('./secrets/token.txt', 'r') as f:
     token = f.readline()
     bot = telebot.TeleBot(token)
     is_random = False
+    is_choice = False
 
 
 @bot.message_handler(commands=['start'])
@@ -16,13 +18,32 @@ def send_welcome(message):
 @bot.message_handler(commands=['random'])
 def send_welcome(message):
     global is_random
+    global is_choice
+    
+    if is_choice:
+        is_choice = False
+     
     is_random = True
     bot.send_message(message.from_user.id, 'Input max number:')
+
+
+@bot.message_handler(commands=['choice'])
+def send_welcome(message):
+    global is_random
+    global is_choice
+    
+    if is_random:
+        is_random = False
+
+    is_choice = True
+    bot.send_message(message.from_user.id, 'Input options (separated by spaces):')
 
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     global is_random
+    global is_choice
+
     if is_random:
         try:
             max_num = int(message.text)
@@ -35,6 +56,12 @@ def echo_all(message):
             bot.send_message(message.from_user.id, 'Not a correct number!')
         
         is_random = False
+    elif is_choice:
+        options = re.split(' +', message.text)
+        choice = random.choice(options)
+        bot.send_message(message.from_user.id, 'Your choice is ' + choice)
+
+        is_choice = False
     else:
         bot.send_message(message.from_user.id, 'Sorry, I don\'t understand...')
 
